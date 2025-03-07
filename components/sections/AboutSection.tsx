1d9e7cb1043697476db737ad232895b4e8d7d2c4
Joschua Rothenbacher <72493292+josch-r@users.noreply.github.com>
2025-03-07 11:48:16 +0100

    added socials

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { GridLayout } from "@/components/layout/GridLayout";
 import { Button } from "@/components/ui/button";
 import { ArrowUpRight } from "lucide-react";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+
 import Link from "next/link";
 
 export function AboutSection() {
@@ -11,9 +13,33 @@ export function AboutSection() {
         <AboutText />
         <ShortVita />
         <Education />
-        <div className="flex justify-end pt-auto col-span-1 md:col-span-12 md:me-8">
-          <Link href="/about" aria-label="About me">
-            <Button variant="secondary">About me</Button>
+        <div className="flex justify-end gap-2 pt-auto col-span-1 md:col-span-12 md:me-8">
+          <Button
+            variant="outline"
+            size="icon"
+            aria-label="Linkedin Profile"
+            asChild
+          >
+            <a
+              href="https://www.linkedin.com/in/joschua-rothenbacher-431448200/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedinIn />
+            </a>
+          </Button>
+          <Button variant="outline" size="icon" aria-label="GitHub Profile">
+            <a
+              href="https://github.com/josch-r"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+          </Button>
+
+          <Link href="/about" aria-label="Read more">
+            <Button variant="secondary">Read more</Button>
           </Link>
         </div>
       </GridLayout>
@@ -24,7 +50,9 @@ export function AboutSection() {
 function AboutText() {
   return (
     <div className="col-span-1 md:col-span-12 lg:col-span-4 md:mx-8">
-      <h2 className="text-heading-medium !font-normal mb-5" id="about-heading">About me</h2>
+      <h2 className="text-heading-medium !font-normal mb-5" id="about-heading">
+        About me
+      </h2>
       <div className="flex flex-row items-center">
         <Image
           src="/images/joschSketch.png"
@@ -39,11 +67,13 @@ function AboutText() {
         </div>
       </div>
       <p className="text-body-small !text-text-secondary mt-4">
-        I&apos;m a creative technologist exploring the intersection of design
-        and technology to create user-centered solutions. From exploring hybrid
-        experiences to designing meaningful interactions, my work is driven by a
-        passion for creating impactful and thoughtful designs. Explore my
-        projects and see for yourself!
+        I&apos;m a creative technologist passionate about blending design and
+        technology to create human-centered solutions. My journey is driven by
+        curiosity, innovation, and the desire to make a meaningful impact. For
+        me, technology is more than just tools—it&apos;s a way to connect,
+        inspire, and create experiences that matter. I believe in putting humans
+        first, ensuring that every solution, whether digital or analog,
+        addresses real needs with thoughtfulness and care.
       </p>
     </div>
   );
@@ -51,11 +81,11 @@ function AboutText() {
 function ShortVita() {
   const experiences = [
     {
-      year: "2019",
-      role: "Software Developer Intern",
-      company: "Widerstand & Söhne",
-      location: "Ulm, Germany",
-      link: "https://www.widerstandundsoehne.de/",
+      year: "2024",
+      role: "Supervisor Medialab",
+      company: "HfG Schwäbisch Gmünd",
+      location: "Schwäbisch Gmünd, Germany",
+      link: "https://hfg-gmuend.de",
     },
     {
       year: "2022",
@@ -65,11 +95,11 @@ function ShortVita() {
       link: "https://fluur.de",
     },
     {
-      year: "2024",
-      role: "Supervisor Medialab",
-      company: "HfG Schwäbisch Gmünd",
-      location: "Schwäbisch Gmünd, Germany",
-      link: "https://hfg-gmuend.de",
+      year: "2019",
+      role: "Software Developer Intern",
+      company: "Widerstand & Söhne",
+      location: "Ulm, Germany",
+      link: "https://www.widerstandundsoehne.de/",
     },
   ];
 
@@ -106,18 +136,18 @@ function ShortVita() {
 function Education() {
   const education = [
     {
-      year: "2019",
-      degree: "Abitur",
-      company: "Waldorfschule am Illerblick",
-      location: "Ulm, Germany",
-      link: "https://www.waldorfschule-am-illerblick.de/",
-    },
-    {
       year: "2024",
       degree: "B.A. Digital Product Design & Development",
       company: "HfG Schwäbisch Gmünd",
       location: "Schwäbisch Gmünd, Germany",
       link: "https://hfg-gmuend.de",
+    },
+    {
+      year: "2019",
+      degree: "Abitur",
+      company: "Waldorfschule am Illerblick",
+      location: "Ulm, Germany",
+      link: "https://www.waldorfschule-am-illerblick.de/",
     },
   ];
 
